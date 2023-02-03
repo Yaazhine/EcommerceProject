adcc5a24d0a9c92ce0acd97ae2f2a804729ba7dc
Vaidhehi Thiru <vaidehithiru@gmail.comgit config --global user.name VaidhehiK>
2023-02-03 13:48:49 +0530

    updated

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const code = document.querySelector(".code")
 const del = document.querySelector(".del")
 // const ul=docuent.querySelector(".ul")
 code.addEventListener("click", () => {
-    link.style.top = `5 px`;
-    link.style.right = ` 0px`;
+    // link.style.top = `5 px`;
+    // link.style.right = ` 0px`;
+    link.style.visibility = "visible";
     del.addEventListener("click", () => {
         link.remove();
     })
